ab4331be1d64612104fef04c0a1cfc5b76303746
RaanGi <45555635+RaanGi@users.noreply.github.com>
2022-11-16 00:08:35 +0100

    Delete build directory

--- build/CMakeFiles/PSVshell.dir/src/power.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/PSVshell.dir/src/power.c.obj: \
- /home/marcos/Documentos/PSVita/PSVshell/src/power.c \
- /usr/local/vitasdk/lib/gcc/arm-vita-eabi/10.3.0/include/stdbool.h \
- /home/marcos/Documentos/PSVita/PSVshell/src/power.h
