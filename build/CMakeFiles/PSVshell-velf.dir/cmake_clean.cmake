3857f506fdd98b672014a4af817e38bda3ad5a27
RaanGi <45555635+RaanGi@users.noreply.github.com>
2023-02-02 12:35:06 +0100

    Delete build directory

--- build/CMakeFiles/PSVshell-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PSVshell-velf"
-  "PSVshell.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PSVshell-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
